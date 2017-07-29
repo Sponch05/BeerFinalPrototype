907f0d4ec38f5dd7831e8c1bb5cff46d90f1f175
Mothupi Keenan <iamkeenan94@gmail.com>
2017-07-30 01:51:42 +0200

    Syntax added for rotating image buttons.

--- app/src/main/java/com/hacktheworld/beerfinalprototype/MainActivity.java
+++ app/src/main/java/com/hacktheworld/beerfinalprototype/MainActivity.java
@@ -1,11 +1,21 @@
 package com.hacktheworld.beerfinalprototype;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import static android.R.attr.path;
+import static android.view.View.X;
+import static android.view.View.Y;
+
 public class MainActivity extends AppCompatActivity {
+
+    private PropertyValuesHolder pvhY;
+    private PropertyValuesHolder pvhX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,26 +24,91 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void consBtn (View view)
-    {
+    public void consBtn (View view) {
         Intent consump = new Intent(this, Consumption.class);
         startActivity(consump);
+        PropertyValuesHolder tranX = PropertyValuesHolder.ofFloat(View.TRANSLATION_X, path);
+        PropertyValuesHolder tranY = PropertyValuesHolder.ofFloat(View.TRANSLATION_Y, path);
+        Object scaleButton;
+
+        //Instantiating the variables
+        String sb ="scaleButton";
+        String px ="pvhX";
+        String py ="pvhY";
+
+        ObjectAnimator scaleAnimation;
+        scaleAnimation = ObjectAnimator.ofPropertyValuesHolder(sb, pvhX, pvhY);
+        scaleAnimation.setRepeatCount(1);
+        scaleAnimation.setRepeatMode(ValueAnimator.REVERSE);
     }
+
 
     public void drinkBtn (View view)
     {
         Intent drink = new Intent(this, DrinkNowCapture.class);
         startActivity(drink);
+
+        Intent drinkBtn  = new Intent(this, Consumption.class);
+        startActivity(drinkBtn);
+        PropertyValuesHolder tranX = PropertyValuesHolder.ofFloat(View.TRANSLATION_X, path);
+        PropertyValuesHolder tranY = PropertyValuesHolder.ofFloat(View.TRANSLATION_Y, path);
+        Object scaleButton;
+
+        //Instantiating the variables
+        String sb ="scaleButton";
+        String px ="pvhX";
+        String py ="pvhY";
+
+        ObjectAnimator scaleAnimation;
+        scaleAnimation = ObjectAnimator.ofPropertyValuesHolder(sb, pvhX, pvhY);
+        scaleAnimation.setRepeatCount(1);
+        scaleAnimation.setRepeatMode(ValueAnimator.REVERSE);
     }
 
     public void order_Btn (View view)
     {
         // implement order API
+        Intent order_Btn = new Intent(this, DrinkNowCapture.class);
+        startActivity(order_Btn);
+
+        Intent drinkBtn  = new Intent(this, Consumption.class);
+        startActivity(drinkBtn);
+        PropertyValuesHolder tranX = PropertyValuesHolder.ofFloat(View.TRANSLATION_X, path);
+        PropertyValuesHolder tranY = PropertyValuesHolder.ofFloat(View.TRANSLATION_Y, path);
+        Object scaleButton;
+
+        //Instantiating the variables
+        String sb ="scaleButton";
+        String px ="pvhX";
+        String py ="pvhY";
+
+        ObjectAnimator scaleAnimation;
+        scaleAnimation = ObjectAnimator.ofPropertyValuesHolder(sb, pvhX, pvhY);
+        scaleAnimation.setRepeatCount(1);
+        scaleAnimation.setRepeatMode(ValueAnimator.REVERSE);
     }
 
     public void location_Btn (View view)
     {
         //Implement google maps API
+        Intent location_Btn = new Intent(this, DrinkNowCapture.class);
+        startActivity(location_Btn);
+
+        Intent drinkBtn  = new Intent(this, Consumption.class);
+        startActivity(location_Btn);
+        PropertyValuesHolder tranX = PropertyValuesHolder.ofFloat(View.TRANSLATION_X, path);
+        PropertyValuesHolder tranY = PropertyValuesHolder.ofFloat(View.TRANSLATION_Y, path);
+        Object scaleButton;
+
+        //Instantiating the variables
+        String sb ="scaleButton";
+        String px ="pvhX";
+        String py ="pvhY";
+
+        ObjectAnimator scaleAnimation;
+        scaleAnimation = ObjectAnimator.ofPropertyValuesHolder(sb, pvhX, pvhY);
+        scaleAnimation.setRepeatCount(1);
+        scaleAnimation.setRepeatMode(ValueAnimator.REVERSE);
     }
 
 
