0c651c5aefe57e3e25f74ae9ea69dbc21c657acf
Mothupi Keenan <iamkeenan94@gmail.com>
2017-07-29 19:00:50 +0200

    Buttons are given functionalities and login background color is changed.

---
+++ app/src/main/java/com/hacktheworld/beerfinalprototype/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.hacktheworld.beerfinalprototype;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+    private static final String REGISTER_REQUEST_URL = "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+    private Map<String, String> params;
+
+    public RegisterRequest(int idNUmber, String name, String surname, String gender ,String username, String email, String cell, int height, int weight, String password, Response.Listener<String> listener) {
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+		params.put("idNUmber", idNUmber + "");
+        params.put("name", name);
+		params.put("surname", surname);
+		params.put("gender", gender);
+        params.put("username", username);
+		params.put("cell", cell);
+        params.put("height", height + "");
+		params.put("weight", weight + "");
+        params.put("password", password);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
---
+++ app/src/main/java/com/hacktheworld/beerfinalprototype/UserAreaActivity.java
@@ -0,0 +1,8 @@
+package com.hacktheworld.beerfinalprototype;
+
+/**
+ * Created by Mothupii_ on 2017-07-29.
+ */
+
+class UserAreaActivity {
+}
