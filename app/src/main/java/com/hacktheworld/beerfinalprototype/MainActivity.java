af5dc7750cf7205dd6ab70161787b5626f9947dd
Siyammukela <siyammukela@gmail.com>
2017-07-30 02:01:31 +0200

    Megerd and fixed backend

--- app/src/main/java/com/hacktheworld/beerfinalprototype/MainActivity.java
+++ app/src/main/java/com/hacktheworld/beerfinalprototype/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Button;
+
+import com.android.volley.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton loginLink = (ImageButton) findViewById(R.id.ingBtnLocation) ;
+
+        //Redirectig user to login page
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(registerIntent);
+            }
+        });
     }
+
+
 
 
     public void consBtn (View view)
